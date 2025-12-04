835a5bcb0f280ac2dd18b7b9c312aa81b151215a
berke-kutlu <brkekutlu@gmail.com>
2025-12-05 02:43:30 +0300

    Implemented class structure for sender.py

--- sender.py
+++ sender.py
@@ -6,80 +6,79 @@ try:
     from gi.repository import Gst, GLib
 except ValueError as e:
     print("ERROR: GStreamer dependencies missing.")
-    print(f"ERROR_MSG: {e}")
+    print(f"{e}")
     sys.exit(1)
 
+class CameraStreamer: 
+    # 127.0.0.1 = Current Device
+    def __init__(self, ip="127.0.0.1", port=5600):
+        self.ip = ip
+        self.port = port
+        self.pipeline = None
+        self.loop = None
 
-UDP_PORT = 5600
-# If this device 127.0.0.1
-# If another device change to that devices IP
-HOST_IP = "127.0.0.1"
+        Gst.init(None)
 
-# Error process
-def on_error(msg):
-    err, debug_info = msg.parse_error()
-    print(f"ERROR: {err.message}")
-    print(f"DEBUG_INFO: {debug_info}")
-    global pipeline, loop
-    if loop:
-        loop.quit()
+    # Error process
+    def on_error(self, bus, msg):
+        err, debug_info = msg.parse_error()
+        print(f"ERROR: {err.message}")
+        print(f"DEBUG: {debug_info}")
 
-# End of stream process
-def on_eos():
-    print("INFO: Reach EOS (End of Stream).")
-    global pipeline, loop
-    if loop:
-        loop.quit()
+        if self.loop:
+            self.loop.quit()
 
-# Get live feed from camera
-# Encode with H.264 send with UDP
-def run_camera_sender():
-    global pipeline, loop
-    Gst.init(None)
+    # End of stream process
+    def on_eos(self):
+        print("INFO: Reach EOS (End of Stream).")
+        if self.loop:
+            self.loop.quit()
 
-    print("INFO: Camera feed getting ready.")
-    print(f"Feed address: {HOST_IP}:{UDP_PORT}")
-    
-    # Pipeline Structure
-    pipeline_str = (
-        f"autovideosrc ! "
-        f"videoconvert ! "
-        f"x264enc tune=zerolatency bitrate=1000 speed-preset=ultrafast ! "
-        f"rtph264pay config-interval=1 pt=96 ! "
-        f"udpsink host={HOST_IP} port={UDP_PORT}"
-    )   
+    # Get live feed from camera
+    # Encode with H.264 send with UDP
+    def run(self):
+        # Define Pipeline Structure
+        pipeline_str = (
+            f"autovideosrc ! "
+            f"videoconvert ! "
+            f"x264enc tune=zerolatency bitrate=1000 speed-preset=ultrafast ! "
+            f"rtph264pay config-interval=1 pt=96 ! "
+            f"udpsink host={self.ip} port={self.port}"
+        )   
 
-    print(f"\nPipeline: {pipeline_str}")
-    # Create the pipeline
-    try:
-        pipeline = Gst.parse_launch(pipeline_str)
-    except Exception as e:
-        print("ERROR: Pipeline could not be created.")
-        print(f"ERROR_MSG: {e}")
-        return
-    
-    # Bus settings
-    bus = pipeline.get_bus()
-    bus.add_signal_watch()
-    bus.connect("message::error", on_error)
+        print(f"INFO: Sending to {self.ip}:{self.port}")
+        print(f"\nPipeline: {pipeline_str}")
+        # Create the pipeline
+        try:
+            self.pipeline = Gst.parse_launch(pipeline_str)
+        except Exception as e:
+            print("ERROR: Failed to create pipeline")
+            print(f"{e}")
+            return
+        
+        # Bus settings
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message::error", self.on_error)
 
-    # Sınce the camera feed is live EOS is not needed
-    # bus.connect("message::eos", on_eos) 
-    
-    print("INFO: Camera feed is starting. Sending the feed on the background.")
-    pipeline.set_state(Gst.State.PLAYING)
+        # Since the camera feed is live EOS is not needed
+        # bus.connect("message::eos", on_eos) 
+        
+        print("INFO: Camera feed is starting. Sending the feed on the background.")
+        self.pipeline.set_state(Gst.State.PLAYING)
 
-    # Main loop
-    loop = GLib.MainLoop()
-    try:
-        loop.run()
-    except KeyboardInterrupt:
-        print("INFO: Program stopped by user.")
-        pass 
-    finally:
-        print("INFO: Cleaning up resources")
-        pipeline.set_state(Gst.State.NULL)
-        print("INFO: Stream stopped.")
+        # Main loop
+        self.loop = GLib.MainLoop()
+        try:
+            self.loop.run()
+        except KeyboardInterrupt:
+            print("INFO: Program stopped by user.")
+            pass 
+        finally:
+            print("INFO: Cleaning up resources...")
+            self.pipeline.set_state(Gst.State.NULL)
+            print("INFO: Stream stopped.")
 
 if __name__ == "__main__":
-    run_camera_sender()
+    streamer = CameraStreamer()
+    streamer.run()
